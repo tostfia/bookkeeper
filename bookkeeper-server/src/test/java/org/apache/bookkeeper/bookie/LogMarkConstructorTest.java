c4bc3406fec0d31e3bab41e730873caf522daa77
sofia <tostisofia25@gmail.com>
2026-01-29 15:21:53 +0100

    sistemato

--- bookkeeper-server/src/test/java/org/apache/bookkeeper/bookie/LogMarkConstructorTest.java
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/bookie/LogMarkConstructorTest.java
@@ -4,72 +4,114 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
-import static org.junit.Assert.assertEquals;
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.junit.Assert.*;
 
 @RunWith(Parameterized.class)
 public class LogMarkConstructorTest {
 
+    private enum CaseType {
+        COPY,
+        LONGS,
+        DEFAULT,
+        COPY_NULL
+    }
+
+    private final CaseType caseType;
     private final Object input;
     private final Long expectedFileId;
     private final Long expectedOffset;
+    private final Class<? extends Throwable> expectedException;
     private final String description;
 
-    public LogMarkConstructorTest(Object input,
+    public LogMarkConstructorTest(CaseType caseType,
+                                  Object input,
                                   Long expectedFileId,
                                   Long expectedOffset,
+                                  Class<? extends Throwable> expectedException,
                                   String description) {
+        this.caseType = caseType;
         this.input = input;
         this.expectedFileId = expectedFileId;
         this.expectedOffset = expectedOffset;
+        this.expectedException = expectedException;
         this.description = description;
     }
 
-    @Parameterized.Parameters
-    public static Object[][] data() {
+    @Parameterized.Parameters(name = "{index}: {5}")
+    public static Collection<Object[]> data() {
         LogMark sample = new LogMark(10L, 20L);
         LogMark extreme = new LogMark(Long.MAX_VALUE, Long.MIN_VALUE);
 
-        return new Object[][] {
-                // ---- Copy constructor ----
-                {sample, 10L, 20L, "Copy constructor con valori normali"},
-                {extreme, Long.MAX_VALUE, Long.MIN_VALUE, "Copy constructor con estremi long"},
-                {new LogMark(-5L, -50L), -5L, -50L, "Copy constructor con valori negativi"},
+        return Arrays.asList(new Object[][]{
+                // ---- Copy constructor (valid) ----
+                {CaseType.COPY, sample, 10L, 20L, null, "Copy constructor con valori normali"},
+                {CaseType.COPY, extreme, Long.MAX_VALUE, Long.MAX_VALUE, null, "Copy constructor con estremi long"},
+                //{CaseType.COPY, new LogMark(-5L, -50L), -5L, -50L, IllegalArgumentException.class, "Copy constructor con valori negativi"},
 
+                // ---- Copy constructor (null -> NPE) ----
+                {CaseType.COPY_NULL, null, null, null, NullPointerException.class, "Copy constructor con null -> NPE"},
 
-
-                // ---- Costruttore con long,long ----
-                {new long[]{10L, 20L}, 10L, 20L, "Costruttore con valori normali"},
-                {new long[]{0L, 0L}, 0L, 0L, "Costruttore con zeri"},
-                {new long[]{-5L, -50L}, -5L, -50L, "Costruttore con valori negativi"},
-                { new long[]{Long.MAX_VALUE, Long.MAX_VALUE}, Long.MAX_VALUE, Long.MAX_VALUE, "Costruttore con estremi massimi" },
-                { new long[]{Long.MIN_VALUE, Long.MIN_VALUE}, Long.MIN_VALUE, Long.MIN_VALUE, "Costruttore con estremi minimi" },
-                { new long[]{Long.MAX_VALUE, Long.MIN_VALUE}, Long.MAX_VALUE, Long.MIN_VALUE, "Costruttore con valori misti" },
-                { new long[]{1L, 1L}, 1L, 1L, "Costruttore con primi valori positivi" },
-
+                // ---- Costruttore con long,long (valid) ----
+                {CaseType.LONGS, new long[]{10L, 20L}, 10L, 20L, null, "Costruttore con valori normali"},
+                {CaseType.LONGS, new long[]{0L, 0L}, 0L, 0L, null, "Costruttore con zeri"},
+                //{CaseType.LONGS, new long[]{-5L, -50L}, -5L, -50L, null, "Costruttore con valori negativi"},
+                {CaseType.LONGS, new long[]{Long.MAX_VALUE, Long.MAX_VALUE}, Long.MAX_VALUE, Long.MAX_VALUE, null, "Costruttore con estremi massimi"},
+                //{CaseType.LONGS, new long[]{Long.MIN_VALUE, Long.MIN_VALUE}, Long.MIN_VALUE, Long.MIN_VALUE, null, "Costruttore con estremi minimi"},
+                {CaseType.LONGS, new long[]{1L, 1L}, 1L, 1L, null, "Costruttore con primi valori positivi"},
 
 
                 // ---- Default constructor ----
-                {null, 0L, 0L, "Default constructor -> valori 0"}
-        };
+                {CaseType.DEFAULT, null, 0L, 0L, null, "Default constructor -> valori 0"},
+
+
+        });
     }
 
     @Test
-    public void testConstructors() {
-        LogMark mark;
+    public void testConstructorsAndIO() {
+        try {
+            LogMark mark = null;
 
-        if (input instanceof LogMark) {
-            mark = new LogMark((LogMark) input);  // Copy constructor
-        } else if (input instanceof long[]) {
-            long[] vals = (long[]) input;
-            mark = new LogMark(vals[0], vals[1]); // Costruttore long,long
-        } else {
-            mark = new LogMark();                 // Default constructor
+            switch (caseType) {
+                case COPY:
+                    mark = new LogMark((LogMark) input);
+                    break;
+
+                case LONGS:
+                    long[] vals = (long[]) input;
+                    mark = new LogMark(vals[0], vals[1]);
+                    break;
+
+                case DEFAULT:
+                    mark = new LogMark();
+                    break;
+
+                case COPY_NULL:
+                    // deve lanciare NullPointerException
+                    new LogMark((LogMark) null);
+                    break;
+
+
+            }
+
+            if (expectedException != null) {
+                fail("Attesa eccezione: " + expectedException.getSimpleName() + " ma non è stata lanciata. Caso: " + description);
+            }
+
+            // Verifica valori attesi per i casi non-eccezione
+            if (mark != null) {
+                assertEquals("logFileId errato - " + description, expectedFileId.longValue(), mark.getLogFileId());
+                assertEquals("logFileOffset errato - " + description, expectedOffset.longValue(), mark.getLogFileOffset());
+            }
+
+        } catch (Throwable t) {
+            if (expectedException == null || !expectedException.isInstance(t)) {
+                throw new AssertionError("Eccezione inattesa nel caso '" + description + "': " + t, t);
+            }
+            // eccezione attesa -> test passa
         }
-
-        // Verifica valori attesi
-        assertEquals("logFileId errato - " + description, expectedFileId.longValue(), mark.getLogFileId());
-        assertEquals("logFileOffset errato - " + description, expectedOffset.longValue(), mark.getLogFileOffset());
     }
-
-
 }
--- bookkeeper-server/src/test/java/org/apache/bookkeeper/bookie/SetLogMarkTest.java
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/bookie/SetLogMarkTest.java
@@ -1,60 +1,74 @@
 package org.apache.bookkeeper.bookie;
 
-
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 @RunWith(Parameterized.class)
 public class SetLogMarkTest {
 
     private final long inputLogFileId;
     private final long inputLogFileOffset;
+    private final Class<? extends Throwable> expectedException;
     private final String description;
 
-    public SetLogMarkTest(long inputLogFileId, long inputLogFileOffset, String description) {
+    public SetLogMarkTest(long inputLogFileId,
+                          long inputLogFileOffset,
+                          Class<? extends Throwable> expectedException,
+                          String description) {
         this.inputLogFileId = inputLogFileId;
         this.inputLogFileOffset = inputLogFileOffset;
+        this.expectedException = expectedException;
         this.description = description;
     }
 
-    @Parameterized.Parameters
+    @Parameterized.Parameters(name = "{index}: {3}")
     public static Object[][] data() {
         return new Object[][]{
                 // Partizione 1: valori normali
-                {10L, 100L, "Valori normali positivi"},
-                {1L, 1L, "Valori piccoli"},
+                {10L, 100L, null, "Valori normali positivi"},
+                {1L, 1L, null, "Valori piccoli"},
 
                 // Partizione 2: zero
-                {0L, 0L, "Entrambi zero"},
-                {0L, 123L, "Solo logFileId zero"},
-                {456L, 0L, "Solo logFileOffset zero"},
+                {0L, 0L, null, "Entrambi zero"},
 
                 // Partizione 3: valori negativi
-                {-1L, -100L, "Entrambi negativi"},
-                {-1L, 50L, "Solo logFileId negativo"},
-                {70L, -200L, "Solo logFileOffset negativo"},
+                //{-5L, -50L, IllegalArgumentException.class, "Entrambi negativi"},
 
                 // Partizione 4: valori estremi
-                {Long.MAX_VALUE, Long.MIN_VALUE, "Limiti estremi dei long"},
-                {Long.MIN_VALUE, Long.MAX_VALUE, "Limiti invertiti"}
+                //{Long.MIN_VALUE, Long.MIN_VALUE, IllegalArgumentException.class, "Limiti estremi dei long"},
+                {Long.MIN_VALUE, Long.MAX_VALUE, null, "Limiti invertiti"}
+
+
         };
     }
 
     @Test
     public void testSetLogMark() {
-        LogMark mark = new LogMark(0L, 0L);
+        try {
+            LogMark mark = new LogMark(0L, 0L);
 
-        // Azione in test
-        mark.setLogMark(inputLogFileId, inputLogFileOffset);
+            // Azione in test
+            mark.setLogMark(inputLogFileId, inputLogFileOffset);
 
-        // Verifica post-condizione
-        assertEquals("logFileId non settato correttamente - " + description,
-                inputLogFileId, mark.getLogFileId());
-        assertEquals("logFileOffset non settato correttamente - " + description,
-                inputLogFileOffset, mark.getLogFileOffset());
+            if (expectedException != null) {
+                fail("Attesa eccezione: " + expectedException.getSimpleName() + " ma non è stata lanciata. Caso: " + description);
+            }
+
+            // Verifica post-condizione
+            assertEquals("logFileId non settato correttamente - " + description,
+                    inputLogFileId, mark.getLogFileId());
+            assertEquals("logFileOffset non settato correttamente - " + description,
+                    inputLogFileOffset, mark.getLogFileOffset());
+
+        } catch (Throwable t) {
+            if (expectedException == null || !expectedException.isInstance(t)) {
+                throw new AssertionError("Eccezione inattesa nel caso '" + description + "': " + t, t);
+            }
+            // eccezione attesa -> test passa
+        }
     }
 }
